45490d35297c16108a96480346ff629532910402
Tamreena Tashfeen <tamreenatashfeen@gmail.com>
2026-09-26 20:44:43 +0500

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -23,7 +23,7 @@ logo_b64 = get_logo_base64()
 # ---------------------------------------------------------
 # Backend Integration Helpers
 # ---------------------------------------------------------
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://bolocal.onrender.com/"
 
 def send_otp(phone: str, cnic: Optional[str] = None):
     try:
